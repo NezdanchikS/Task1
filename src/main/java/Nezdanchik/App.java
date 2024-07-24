57d1f0199a1df13344120c5afcd33ce6b1427687
NeazdanchikS <NezdanchikS@yandex.ru>
2024-07-24 23:10:17 +0300

    Initial test commit

---
+++ src/main/java/Nezdanchik/App.java
@@ -0,0 +1,13 @@
+package Nezdanchik;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
